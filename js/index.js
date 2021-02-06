9d8c7975fd6150d7358b860c4950a2b00380872a
Kevin Turkington <kevin.turkington@compass.com>
2021-02-05 23:56:15 -0800

    website revamp

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-const gClickNav = function(resouceName) {
-  gtag('event', 'click', {
-    'event_category': 'outbound',
-    'event_label': resouceName,
-    'transport_type': 'beacon'
-  })
-}
--- update_resume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -O downloadables/KevinTurkington_Resume.pdf "https://github.com/zainkai/Resume/raw/master/KevinTurkington.pdf"
